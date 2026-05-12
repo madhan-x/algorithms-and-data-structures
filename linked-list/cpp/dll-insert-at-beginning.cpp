fb2bd54ab894b4ca7b59f2733cab0b5e8a7cb824
Madhan Thalloju <madhanthalloju@gmail.com>
2026-05-12 20:01:45 +0530

    Implement insert at beginning for doubly linked list
    This implementation allows users to insert nodes at the beginning of a doubly linked list and display the list.

---
+++ linked-list/cpp/dll-insert-at-beginning.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+
+// Node class for Doubly Linked List
+class Node
+{
+public:
+    int data;
+    Node* prev;
+    Node* next;
+
+    // Constructor
+    Node(int val)
+    {
+        data = val;
+        prev = NULL;
+        next = NULL;
+    }
+};
+
+// Function to insert node at beginning
+void insertBegin(Node* &head, int val)
+{
+    // Create new node
+    Node* newNode = new Node(val);
+
+    // If list is not empty
+    if(head != NULL)
+    {
+        newNode->next = head;
+        head->prev = newNode;
+    }
+
+    // Move head to new node
+    head = newNode;
+}
+
+// Function to display DLL
+void display(Node* head)
+{
+    Node* temp = head;
+
+    while(temp != NULL)
+    {
+        cout << temp->data << " <-> ";
+        temp = temp->next;
+    }
+
+    cout << "NULL";
+}
+
+int main()
+{
+    Node* head = NULL;
+
+    int n, val;
+
+    cout << "Enter number of nodes: ";
+    cin >> n;
+
+    // Input nodes from user
+    for(int i = 0; i < n; i++)
+    {
+        cout << "Enter data: ";
+        cin >> val;
+
+        insertBegin(head, val);
+    }
+
+    cout << "\nDoubly Linked List:\n";
+    display(head);
+
+    return 0;
+}
