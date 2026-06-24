111c67990ab4042b542c837b6de073df0675f7c9
Madhan <madhanthalloju@gmail.com>
2026-06-24 17:42:23 +0530

    Implement brute force solution for Two Sum problem

---
+++ leetcode/arrays/easy/two-sum-bruteforce.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int>& nums, int target)
+    {
+        // Check every pair
+        for(int i = 0; i < nums.size(); i++)
+        {
+            for(int j = i + 1; j < nums.size(); j++)
+            {
+                if(nums[i] + nums[j] == target)
+                {
+                    return {i, j};
+                }
+            }
+        }
+
+        return {};
+    }
+};
