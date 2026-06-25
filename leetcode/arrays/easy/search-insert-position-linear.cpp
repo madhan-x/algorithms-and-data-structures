881d8403a605b0cbbba68a50f7bc736c231b6e2f
Madhan <madhanthalloju@gmail.com>
2026-06-25 07:44:37 +0530

    Implement searchInsert function for array

---
+++ leetcode/arrays/easy/search-insert-position-linear.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target)
+{
+
+        // Traverse the array
+        for(int i = 0; i < nums.size(); i++)
+        {
+            // Target found
+            if(nums[i] == target)
+            {
+                return i;
+            }
+
+            // Target should be inserted here
+            if(nums[i] > target)
+            {
+                return i;
+            }
+        }
+
+        // Insert at the end
+        return nums.size();
+    }
+};
