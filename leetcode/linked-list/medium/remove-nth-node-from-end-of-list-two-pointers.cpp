843a84f3d6a0ee901f043fabee4518c02244e94c
Madhan <madhanthalloju@gmail.com>
2026-07-03 00:07:41 +0530

    Create remove-nth-node-from-end-of-list-two-pointers.cpp

---
+++ leetcode/linked-list/medium/remove-nth-node-from-end-of-list-two-pointers.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution
+{
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n)
+    {
+        // Create a dummy node
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+
+        // Initialize two pointers
+        ListNode* fast = dummy;
+        ListNode* slow = dummy;
+
+        // Move fast pointer n+1 steps ahead
+        for(int i = 0; i <= n; i++)
+        {
+            fast = fast->next;
+        }
+
+        // Move both pointers until fast reaches the end
+        while(fast != nullptr)
+        {
+            fast = fast->next;
+            slow = slow->next;
+        }
+
+        // Delete the nth node from the end
+        ListNode* temp = slow->next;
+        slow->next = slow->next->next;
+        delete temp;
+
+        return dummy->next;
+    }
+};
